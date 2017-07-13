ecd212e7db3ea139cd826e97a4094bac3de7aa15
Adam Howard <skattyadz+gh@gmail.com>
2017-07-13 09:23:05 -0400

    Read a project from Contentful

--- pages/projects.js
+++ pages/projects.js
@@ -1,36 +1,46 @@
 import { Component } from 'react'
 import Head from 'next/head'
+
+import contentful from '~/lib/contentful'
 import Header from '~/components/header'
 
 export default class extends Component {
   static async getInitialProps ({ req }) {
-    // return req
-    //   ? { userAgent: req.headers['user-agent'] }
-    // : { userAgent: navigator.userAgent }
+    return contentful.getEntries({
+      content_type: 'project',
+      'fields.slug': 'weights-and-measures',
+      limit: 1
+    })
+      .then((entries) => ({
+        project: entries.items[0].fields
+      }))
   }
 
-  render() {
-    <div>
-      <Head>
-        <title>Projects</title>
-      </Head>
-      <Header />
+  render () {
+    const { project } = this.props
 
-      Hello world
-      <p>scoped!</p>
-      <style jsx>{`
-        p {
-          color: blue;
-        }
-        div {
-          background: red;
-        }
-        @media (max-width: 600px) {
-          div {
-            background: blue;
+    return (
+      <div>
+        <Head>
+          <title>Projects</title>
+        </Head>
+        <Header />
+
+        <p>{project.introduction}</p>
+        <style jsx>{`
+          p {
+            color: blue;
           }
-        }
-      `}</style>
-    </div>
+          div {
+            background: red;
+          }
+          @media (max-width: 600px) {
+            div {
+              background: blue;
+            }
+          }
+        `}</style>
+      </div>
+    )
   }
 }
