9db26d4a2b0b232e466128c797953ecf5873358b
Adam Howard <skattyadz+gh@gmail.com>
2017-07-31 00:09:40 -0400

    Add masonry-style homepage scrolling

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,21 @@
 import { Component } from "react";
 import Head from "next/head";
+import { Element as ScrollElement } from "react-scroll";
 
 import { getClient } from "~/lib/contentful";
+import { scrollNameForYear, YEARS } from "~/lib/scrollNames";
 import Container from "~/components/Container";
 import Header from "~/components/Header";
 import ImageSlider from "~/components/ImageSlider";
-import ProjectPreview from "~/components/ProjectPreview";
+import Masonry from "~/components/Masonry";
 import Sidebar from "~/components/Sidebar";
-
-const scrollNameForYear = year => `year-${year}`;
-
-const scrollNameForProject = project => {
-  const year = new Date(project.date).getFullYear();
-  const closestYear = YEARS.find(y => y <= year);
-  return scrollNameForYear(closestYear);
-};
-
-const thisYear = new Date().getFullYear();
-const YEARS = [thisYear, 2010, 2005, 2000, 1995, 1990];
 
 export default class extends Component {
   static async getInitialProps({ query }) {
-    const entries = await getClient({ preview: !!query.preview }).getEntries(
-      { content_type: "project" },
-      { sort: "date" }
-    );
+    const entries = await getClient({ preview: !!query.preview }).getEntries({
+      content_type: "project",
+      order: "-fields.date",
+    });
     return {
       projects: entries.items.map(item => item.fields),
     };
@@ -42,8 +33,8 @@ export default class extends Component {
         <Header />
 
         <Sidebar
-          items={YEARS.map(year => ({
-            label: year,
+          items={YEARS.map((year, i) => ({
+            label: i === 0 ? "Current" : year,
             scrollName: scrollNameForYear(year),
           }))}
         />
@@ -51,15 +42,32 @@ export default class extends Component {
         <Container>
           <ImageSlider item={{ images: [] }} />
 
-          {projects.map(project =>
-            <ProjectPreview
-              project={project}
-              scrollName={scrollNameForProject(project)}
-            />
+          {YEARS.map((year, i) =>
+            <ScrollElement name={scrollNameForYear(year)}>
+              <div className={i === 0 ? "firstSection" : null}>
+                <Masonry
+                  projects={projects.filter(p => {
+                    const projectYear = new Date(p.date).getFullYear();
+                    const nextYear = YEARS[i + 1] || 1900;
+                    return projectYear <= year && projectYear > nextYear;
+                  })}
+                />
+              </div>
+            </ScrollElement>
           )}
         </Container>
 
-        <style jsx>{``}</style>
+        <div className="spacer" />
+
+        <style jsx>{`
+          .spacer {
+            height: 100vh;
+          }
+          .firstSection {
+            padding-top: 300px;
+            margin-top: -300px;
+          }
+        `}</style>
       </div>
     );
   }
