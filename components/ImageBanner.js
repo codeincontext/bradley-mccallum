e995331e83e6f050ac93da447c2eac7f3f502e6f
Adam Howard <skattyadz+gh@gmail.com>
2017-08-06 09:23:58 -0400

    Add initial image banner

--- components/ImageBanner.js
+++ components/ImageBanner.js
@@ -1,6 +1,29 @@
 import Image from "~/components/Image";
 
 export default ({ item }) =>
-  <div>
-    {item.images.map(image => <Image image={image} />)}
+  <div className="root">
+    {item.images.map(image =>
+      <div
+        className="image"
+        style={{
+          flexGrow:
+            image.fields.file.details.image.width /
+            image.fields.file.details.image.height,
+        }}
+      >
+        <Image image={image} />
+      </div>
+    )}
+
+    <style jsx>{`
+      .root {
+        width: 100%;
+        display: flex;
+        // height: 10vw;
+        align-items: stretch;
+      }
+      .image {
+        flex-basis: 0;
+      }
+    `}</style>
   </div>;
