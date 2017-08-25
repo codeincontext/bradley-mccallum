6c4ab4f4c27b6d703251232c9b850ddcb1aca805
Adam Howard <skattyadz+gh@gmail.com>
2017-08-25 11:18:17 -0400

    Fix single image

--- components/SingleImage.js
+++ components/SingleImage.js
@@ -1,10 +1,11 @@
 import Image from "~/components/Image";
 import Container from "~/components/Container";
+import RichText from "~/components/RichText";
 
 export default ({ item }) =>
   <Container>
     <Image image={item.image.url} />
     <p>
-      {item.caption}
+      <RichText text={item.caption} />
     </p>
   </Container>;
