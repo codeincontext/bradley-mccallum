d1926f85d3f0a1631ed998efe10cc2f1acee516d
Adam Howard <skattyadz+gh@gmail.com>
2017-08-31 13:51:10 -0400

    Add press items to project and bibliography pages

---
+++ components/PressItem.js
@@ -0,0 +1,41 @@
+import { RichText } from 'prismic-dom';
+import Link from 'next/link';
+import { weights } from '~/components/theme';
+
+const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+function formatDate(date) {
+  const d = new Date(date);
+  return `${MONTH_NAMES[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+}
+
+export default ({ item: { author, title, publication, date } }) => (
+  <li>
+    {author}, &ldquo;<Link>
+      <a>{title},</a>
+    </Link>&rdquo; {RichText.asText(publication)}, {formatDate(date)}
+    <style jsx>{`
+      li {
+        list-style: none;
+      }
+      a {
+        font-weight: ${weights.bold};
+        text-decoration: underline;
+        text-decoration-skip: ink;
+      }
+    `}</style>
+  </li>
+);
