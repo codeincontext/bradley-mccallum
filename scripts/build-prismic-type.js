52bee893d5a61fe89af6f84284c1883393c3d203
Adam Howard <skattyadz+gh@gmail.com>
2017-08-31 16:43:05 -0400

    Catch files missed from prettier update

--- scripts/build-prismic-type.js
+++ scripts/build-prismic-type.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const filePath = process.argv[2];
-const source = fs.readFileSync(filePath, "utf8");
-const slices = fs.readFileSync("scripts/prismic-types/slices.json", "utf8");
+const source = fs.readFileSync(filePath, 'utf8');
+const slices = fs.readFileSync('scripts/prismic-types/slices.json', 'utf8');
 
 const output = source.replace(/%%CHOICES%%/g, slices);
 console.log(output);
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 // NOTE: We only need a custom server.js for the project/:slug route
 
-const express = require("express");
-const next = require("next");
+const express = require('express');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
-  server.get("/project/:slug", (req, res) =>
+  server.get('/project/:slug', (req, res) =>
     app.render(
       req,
       res,
-      "/project",
+      '/project',
       Object.assign({ slug: req.params.slug }, req.query)
     )
   );
 
-  server.get("*", (req, res) => handle(req, res));
+  server.get('*', (req, res) => handle(req, res));
 
   server.listen(3000, err => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log('> Ready on http://localhost:3000');
   });
 });
