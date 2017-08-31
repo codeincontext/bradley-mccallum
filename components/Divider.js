46f8af296a44dc0bf56f39ab4d6eaf6d721b6988
Adam Howard <skattyadz+gh@gmail.com>
2017-08-31 17:31:57 -0400

    Style Divider properly

--- components/Divider.js
+++ components/Divider.js
@@ -1,3 +1,20 @@
+import { colors, spacing } from '~/lib/theme';
 import Container from '~/components/Container';
 
-export default () => <Container>---</Container>;
+export default () => (
+  <div>
+    <Container>
+      <hr />
+    </Container>
+
+    <style jsx>{`
+      hr {
+        border: none;
+        width: 100%;
+        height: 1px;
+        background-color: ${colors.dividerGrey};
+        margin: ${spacing.s2} 0;
+      }
+    `}</style>
+  </div>
+);
