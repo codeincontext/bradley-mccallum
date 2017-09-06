e092ad68d384abb520864faa1d6869df8c5ea022
Adam Howard <skattyadz+gh@gmail.com>
2017-09-06 16:42:28 -0400

    Fix heading indentation

--- components/MainHeading.js
+++ components/MainHeading.js
@@ -5,25 +5,25 @@ const MainHeading = ({ children }) => (
     <h1>{children}</h1>
 
     <style jsx>{`
-        .heading {
-          background: radial-gradient(${colors.black} 15%, transparent 16%),
-            radial-gradient(${colors.black} 15%, transparent 16%),
-            ${colors.lightGrey};
-          background-size: 7px 7px;
-          padding-top: ${spacing.s2};
-          margin-bottom: ${spacing.s2};
-        }
-        .heading h1 {
-          font-weight: ${weights.light};
-          font-size: ${fonts.f24};
-          text-transform: uppercase;
-          letter-spacing: 2px;
-          display: inline-block;
-          margin: 0;
-          padding: ${spacing.s05} ${spacing.s1} 0 ${spacing.s025};
-          background-color: ${colors.lightGrey};
-        }
-      `}</style>
+      .heading {
+        background: radial-gradient(${colors.black} 15%, transparent 16%),
+          radial-gradient(${colors.black} 15%, transparent 16%),
+          ${colors.lightGrey};
+        background-size: 7px 7px;
+        padding-top: ${spacing.s2};
+        margin-bottom: ${spacing.s2};
+      }
+      .heading h1 {
+        font-weight: ${weights.light};
+        font-size: ${fonts.f24};
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        display: inline-block;
+        margin: 0;
+        padding: ${spacing.s05} ${spacing.s1} 0 ${spacing.s025};
+        background-color: ${colors.lightGrey};
+      }
+    `}</style>
   </div>
 );
 
