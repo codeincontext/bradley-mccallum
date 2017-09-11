2db71c9b050334a8b2d6c2fff54cd0c3ac6d8ad9
Adam Howard <skattyadz+gh@gmail.com>
2017-09-11 17:47:48 -0400

    Add Embed component

--- components/Embed.js
+++ components/Embed.js
@@ -1,5 +1,17 @@
 import Container from '~/components/Container';
 
-const Embed = ({ item }) => <Container />;
+const Embed = ({ item: { embed } }) => {
+  return (
+    <Container>
+      <div dangerouslySetInnerHTML={{ __html: embed.html }} />
+
+      <style jsx>{`
+        div > :global(*) {
+          width: 100%;
+        }
+      `}</style>
+    </Container>
+  );
+};
 
 export default Embed;
