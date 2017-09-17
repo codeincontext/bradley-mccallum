775f51e29291cd659cfbcd09c19e3acf2710536b
Adam Howard <skattyadz+gh@gmail.com>
2017-09-17 10:02:36 +0100

    Give image components the correct height before the image has loaded to prevernt page jump

--- components/Image.js
+++ components/Image.js
@@ -1,15 +1,25 @@
 const Image = ({ image }) => {
-  if (!image) return null;
+  if (!image || !Object.keys(image).length) return null;
+  const { url, alt, dimensions } = image;
 
   return (
-    <div>
+    <div
+      className="root"
+      style={{ paddingTop: `${dimensions.height / dimensions.width * 100}%` }}
+    >
       <img src={image.url} alt={image.alt} />
 
       <style jsx>{`
+        .root {
+          position: relative;
+        }
+
         img {
           display: block;
-          max-width: 100%;
-          margin: 0;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
         }
       `}</style>
     </div>
