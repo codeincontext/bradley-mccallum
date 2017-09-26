fdcc6ad0af9c1ffe86624232a5912484d072fea9
Adam Howard <skattyadz+gh@gmail.com>
2017-09-26 16:54:26 +0100

    Fix image banner height

--- components/ContentItem/ImageBanner.js
+++ components/ContentItem/ImageBanner.js
@@ -1,34 +1,77 @@
 import Image from '~/components/Image';
 import { CONTENT_ITEM_SPACING } from '~/lib/theme';
 
-const ImageBanner = ({ item }) => (
-  <div className="root">
-    {item.items.filter(({ image }) => image.url).map(({ image }) => {
-      const { width, height } = image.dimensions;
-      return (
-        <div
-          className="image"
-          style={{ flexGrow: width / height }}
-          key={image.url}
-        >
-          <Image image={image} />
-        </div>
-      );
-    })}
+const BANNER_HEIGHT = 200;
 
-    <style jsx>{`
-      .root {
-        width: 100%;
-        display: flex;
-        // height: 10vw;
-        align-items: stretch;
-        margin-bottom: ${CONTENT_ITEM_SPACING};
-      }
-      .image {
-        flex-basis: 0;
-      }
-    `}</style>
-  </div>
-);
+{
+  /*
+   By default images have a fixed size and therefore the banner has a fixed width
+   It looks strange if the window size is similar to the banner width: there's a small scrollbar or a small gap.
+
+   Use MQs to prevent this:
+   - Window thinner than banner: Set correct width for 200px height
+   - Window is close to the ideal banner size: Use flexbox to fit to size
+   - Window wider than banner: Set correct width for 200px height
+ */
+}
+const cssForImage = ({ width, height, className, totalWidth }) => `
+  .${className} {
+    width: ${width / height * BANNER_HEIGHT}px;
+  }
+
+  @media (min-width: ${totalWidth * 0.9}px) {
+    .${className} {
+      width: auto;
+      flex-basis: 0;
+      flex-grow: ${width / height};
+    }
+  }
+
+  @media (min-width: ${totalWidth * 1.1}px) {
+    .${className} {
+      width: ${width / height * BANNER_HEIGHT}px;
+    }
+  }
+`;
+
+const ImageBanner = ({ item }) => {
+  const images = item.items
+    .map(({ image }) => image)
+    .filter(image => image.url);
+
+  const totalWidth = images.reduce(
+    (total, image) =>
+      total + image.dimensions.width / image.dimensions.height * BANNER_HEIGHT,
+    0
+  );
+
+  return (
+    <div className="root">
+      {images.map(image => {
+        const { width, height } = image.dimensions;
+        const className = `banner-image-${width}-${height}`;
+        return (
+          <div className={`image ${className}`} key={image.url}>
+            <Image image={image} />
+            <style>
+              {cssForImage({ width, height, className, totalWidth })}
+            </style>
+          </div>
+        );
+      })}
+
+      <style jsx>{`
+        .root {
+          display: flex;
+          overflow-x: auto;
+          margin-bottom: ${CONTENT_ITEM_SPACING};
+        }
+        .image {
+          flex-shrink: 0;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default ImageBanner;
