9e70543138f8964affe9eb25272529ba6578ccab
Adam Howard <skattyadz+gh@gmail.com>
2017-10-01 19:53:39 +0100

    Get server port from env var

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.prepare().then(() => {
 
   server.get('*', (req, res) => handle(req, res));
 
-  server.listen(3000, err => {
+  server.listen(process.env.PORT || 3000, err => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
   });
