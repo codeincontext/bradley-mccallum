af7d3b51fafb5ea0a2f4fa98392872868e7953c7
Adam Howard <skattyadz+gh@gmail.com>
2017-10-29 14:22:49 +0000

    Use large/small dropdown on heading

--- components/ContentItem/Subheading.js
+++ components/ContentItem/Subheading.js
@@ -10,36 +10,40 @@ import {
 } from '~/lib/theme';
 import Container from '~/components/Container';
 
-const Subheading = ({ item: { type, text } }) => (
-  <Container>
-    <div
-      className="root"
-      dangerouslySetInnerHTML={{ __html: PrismicDom.RichText.asHtml(text) }}
-    />
+const Subheading = ({ item: { size, text } }) => {
+  const HeadingSize = size === 'small' ? 'h4' : 'h3';
+  return (
+    <Container>
+      <div className="root">
+        <HeadingSize className="root">
+          {PrismicDom.RichText.asText(text)}
+        </HeadingSize>
+      </div>
 
-    <style jsx>{`
-      .root > :global(h3),
-      .root > :global(h4) {
-        text-transform: uppercase;
-        line-height: ${lineHeights.heading};
-        letter-spacing: ${letterSpacing.loose};
-      }
+      <style jsx>{`
+        .root > :global(h3),
+        .root > :global(h4) {
+          text-transform: uppercase;
+          line-height: ${lineHeights.heading};
+          letter-spacing: ${letterSpacing.loose};
+        }
 
-      .root > :global(h3) {
-        margin: 0 auto ${CONTENT_ITEM_SPACING};
-        max-width: ${CALLOUT_WIDTH}%;
-        font-size: ${fonts.f30};
-        font-weight: ${weights.light};
-        text-align: center;
-      }
+        .root > :global(h3) {
+          margin: 0 auto ${CONTENT_ITEM_SPACING};
+          max-width: ${CALLOUT_WIDTH}%;
+          font-size: ${fonts.f30};
+          font-weight: ${weights.light};
+          text-align: center;
+        }
 
-      .root > :global(h4) {
-        margin: 0 auto ${spacing.s1};
-        font-size: ${fonts.f15};
-        font-weight: ${weights.bold};
-      }
-    `}</style>
-  </Container>
-);
+        .root > :global(h4) {
+          margin: 0 auto ${spacing.s1};
+          font-size: ${fonts.f15};
+          font-weight: ${weights.bold};
+        }
+      `}</style>
+    </Container>
+  );
+};
 
 export default Subheading;
