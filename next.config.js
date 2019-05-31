e1f8a6e15da8c528b71e3058be406322e13d4db9
Adam Howard <skattyadz+gh@gmail.com>
2019-05-31 12:55:12 +0200

    Remove contentful (we use prismic now)

--- next.config.js
+++ next.config.js
@@ -6,14 +6,4 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
   target: 'serverless',
-
-  env: {
-    CONTENTFUL_SPACE: JSON.stringify(process.env.CONTENTFUL_SPACE),
-    CONTENTFUL_ACCESS_TOKEN: JSON.stringify(
-      process.env.CONTENTFUL_ACCESS_TOKEN
-    ),
-    CONTENTFUL_PREVIEW_TOKEN: JSON.stringify(
-      process.env.CONTENTFUL_PREVIEW_TOKEN
-    ),
-  },
 };
